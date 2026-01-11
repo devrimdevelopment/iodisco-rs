c3075dd2293eeb3a88519303b0b6b57dc2918009
devrimdevelopment <devrimdevelopment@gmail.com>
2026-01-11 18:59:54 +0000

    gpuinfo binary fixed

--- gpuinfo-bin/src/main.rs
+++ gpuinfo-bin/src/main.rs
@@ -4,32 +4,7 @@ use std::process;
 
 fn main() {
     match iodisco::get_gpu_info() {
-        Ok(gpu) => {
-            // One-line output
-            print!("{} {}", gpu.vendor, gpu.model);
-
-            if let Some(cores) = gpu.cores {
-                print!(" ({} cores)", cores);
-            }
-
-            if let Some(arch) = gpu.architecture {
-                print!(" [{}]", arch);
-            }
-
-            println!();
-            process::exit(0);
-        }
-        Err(iodisco::GpuInfoError::NoDevice) => {
-            eprintln!("no_gpu");
-            process::exit(1);
-        }
-        Err(iodisco::GpuInfoError::NoProfile) => {
-            eprintln!("unknown_gpu");
-            process::exit(2);
-        }
-        Err(e) => {
-            eprintln!("error: {}", e);
-            process::exit(3);
-        }
+        Ok(gpu) => println!("{} {}", gpu.vendor, gpu.model),
+        Err(_) => std::process::exit(1),
     }
 }
